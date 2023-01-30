2f1ba68fdab480fe601cf052ec730c213989677a
Filip Popovski <filippopovski1998@gmail.com>
2023-01-30 20:10:27 +0100

    add antialiasing

--- main.cpp
+++ main.cpp
@@ -1,43 +1,12 @@
-#ifndef RTWEEKEND_H
-#define RTWEEKEND_H
-
-#include <cmath>
-#include <limits>
-#include <memory>
-
-
-// Usings
-
-using std::shared_ptr;
-using std::make_shared;
-using std::sqrt;
-
-// Constants
-
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi = 3.1415926535897932385;
-
-// Utility Functions
-
-inline double degrees_to_radians(double degrees) {
-    return degrees * pi / 180.0;
-}
-
-// Common Headers
-
-#include "ray.h"
-#include "vec3.h"
-
-#endif
-
 #include "rtweekend.h"
 
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
-#include <fstream>
+#include <random>
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
@@ -68,6 +37,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -76,14 +46,7 @@ int main() {
 
     // Camera
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
 
@@ -94,11 +57,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(out, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(out, pixel_color, samples_per_pixel);
         }
     }
     std::cerr << "\nDone.\n";
--- rtweekend.h
+++ rtweekend.h
@@ -4,7 +4,12 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
+// Common Headers
+
+#include "ray.h"
+#include "vec3.h"
 
 // Usings
 
@@ -23,9 +28,20 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-// Common Headers
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
 
-#include "ray.h"
-#include "vec3.h"
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
 
 #endif
