3afc83358484e8a10479befd5dfe281a09833680
Filip Popovski <filippopovski1998@gmail.com>
2023-02-07 20:43:28 +0100

    standard cornell box scene

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "material.h"
 #include "moving_sphere.h"
 #include "aarect.h"
+#include "box.h"
 
 #include <iostream>
 #include <random>
@@ -146,9 +147,19 @@ hittable_list cornell_box() {
     objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
     objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
     objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
-    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
 
     return objects;
 }
